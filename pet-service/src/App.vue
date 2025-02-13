e726d24c25714e24e3f8b5e9d30ef57852faf80d
최수현 <sw92522@naver.com>
2025-02-13 13:50:37 +0900

    init: create project

---
+++ pet-service/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import NavigationBar from "./components/NavigationBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavigationBar,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <NavigationBar />
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+</style>
